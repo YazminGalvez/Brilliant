5f21b13b9debfbd52159d2c2c122bd205541948e
Yazmin Galvez <yazgalvez1515@gmail.com>
2026-09-10 12:37:46 -0700

    Inicio Brilliant

---
+++ bin/juego_brilliant.dart
@@ -0,0 +1,5 @@
+import 'package:juego_brilliant/juego_brilliant.dart' as juego_brilliant;
+
+void main(List<String> arguments) {
+  print('Hello world: ${juego_brilliant.calculate()}!');
+}
---
+++ test/juego_brilliant_test.dart
@@ -0,0 +1,8 @@
+import 'package:juego_brilliant/juego_brilliant.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
